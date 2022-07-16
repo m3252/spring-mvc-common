8b6565606476f7c5609cf5a006e7f82530126633
noah <m3252@naver.com>
2022-07-16 20:02:38 +0900

    refactor : ValidationUtils 사용해보기, 메시지 리졸버 확인

--- validation/src/main/java/hello/itemservice/web/validation/ValidationItemControllerV2.java
+++ validation/src/main/java/hello/itemservice/web/validation/ValidationItemControllerV2.java
@@ -10,6 +10,7 @@ import org.springframework.util.StringUtils;
 import org.springframework.validation.BindingResult;
 import org.springframework.validation.FieldError;
 import org.springframework.validation.ObjectError;
+import org.springframework.validation.ValidationUtils;
 import org.springframework.web.bind.annotation.*;
 import org.springframework.web.servlet.mvc.support.RedirectAttributes;
 
@@ -181,11 +182,13 @@ public class ValidationItemControllerV2 {
         log.info("objectName = {}", bindingResult.getObjectName());
         log.info("target = {}", bindingResult.getTarget());
 
-        //상품 이름이 있나요?
+        //상품 이름이 있나요? ValidationUtils.rejectIfEmpty(bindingResult, "itemName", "required"); 와 동일하게 동작한다.
         if (!StringUtils.hasText(item.getItemName())) {
             bindingResult.rejectValue("itemName", "required");
 //            new String[]{"required.item.itemName", "required"};
+
         }
+
 
         if (item.getPrice() == null || item.getPrice() < 1000 || item.getPrice() > 1000000) {
             bindingResult.rejectValue("price", "range", new Object[]{1000, 1000000}, null);
