d78cc7d98ab94cc317f96dfd55dc2865cc436b94
noah <m3252@naver.com>
2022-08-01 23:58:47 +0900

    feat : 오류처리 방법1. @ResponseStatus

--- exception/src/main/java/hello/exception/api/ApiExceptionController.java
+++ exception/src/main/java/hello/exception/api/ApiExceptionController.java
@@ -1,5 +1,6 @@
 package hello.exception.api;
 
+import hello.exception.ex.BadRequestException;
 import hello.exception.ex.UserException;
 import lombok.AllArgsConstructor;
 import lombok.Data;
@@ -24,6 +25,11 @@ public class ApiExceptionController {
         }
 
         return new MemberDto(id, "hello " + id);
+    }
+
+    @GetMapping("/api/response-status-ex1")
+    public String responseStatusEx1() {
+        throw new BadRequestException();
     }
 
     @Data
